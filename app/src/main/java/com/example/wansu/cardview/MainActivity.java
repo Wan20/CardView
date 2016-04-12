f9d914ad2a8e0fd60ee8761825bdfdc44ddbd9c2
Wan20 <wandy20number@gmail.com>
2016-04-12 15:24:47 +0700

    Edit CardView

--- app/src/main/java/com/example/wansu/cardview/MainActivity.java
+++ app/src/main/java/com/example/wansu/cardview/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.wansu.cardview;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -17,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        CardView cardView = (CardView)findViewById(R.id.cv_main);
+        cardView.setCardElevation(0.5f);
 
     }
 
