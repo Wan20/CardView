1733c012f0df83ecfca47a7992305f8c65f81255
Wan20 <wandy20number@gmail.com>
2016-04-12 19:40:12 +0700

    Background Color

--- app/src/main/java/com/example/wansu/cardview/MainActivity.java
+++ app/src/main/java/com/example/wansu/cardview/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.wansu.cardview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
